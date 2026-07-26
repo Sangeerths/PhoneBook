34040d5fe6bbb106df8a43a8d3e4645c0aa1e5b3
sange <sange@SANGEERTH>
2026-07-26 23:37:09 +0530

    Add ConsoleMenu class and clean up project file
    Introduced ConsoleMenu class with menu options enum for UI handling. Removed "Controller" folder reference from PhoneBook.csproj to clean up project structure.

---
+++ PhoneBook/UI/ConsoleMenu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBook.UI
+{
+    public class ConsoleMenu
+    {
+        private enum MenuOptions
+        {
+            InsertContact,
+            DeleteContact,
+            ModifyContact,
+            ViewContact,
+            ViewAllContacts,
+            Exit
+        }
+    }
+}
