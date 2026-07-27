dd376b9764670e5775a172f0156c4135ac445d78
sange <sange@SANGEERTH>
2026-07-27 10:08:40 +0530

    Integrate Spectre.Console and refactor menu system
    Added Spectre.Console NuGet package for enhanced console UI. Refactored ConsoleMenu to use SelectionPrompt for interactive menus. Introduced OnStart() method to handle menu logic. Added method stubs for contact operations for future development.

--- PhoneBook/UI/ConsoleMenu.cs
+++ PhoneBook/UI/ConsoleMenu.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Spectre.Console;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace PhoneBook.UI
 {
@@ -14,6 +16,65 @@ namespace PhoneBook.UI
             ViewContact,
             ViewAllContacts,
             Exit
+        }
+
+        public ConsoleMenu() { }
+
+        public void OnStart()
+        {
+            bool isRunning = true;
+            while  (isRunning)
+            {
+                var choice = AnsiConsole.Prompt(new SelectionPrompt<MenuOptions>().Title("Choose your operation").AddChoices(
+                    MenuOptions.InsertContact,
+                    MenuOptions.DeleteContact,
+                    MenuOptions.ModifyContact,
+                    MenuOptions.ViewContact,
+                    MenuOptions.ViewAllContacts,
+                    MenuOptions.Exit));
+
+                switch (choice)
+                {
+                    case MenuOptions.InsertContact:
+                        break;
+                    case MenuOptions.DeleteContact:
+                        break;
+                    case MenuOptions.ModifyContact:
+                        break;
+                    case MenuOptions.ViewContact:
+                        break;
+                    case MenuOptions.ViewAllContacts:
+                        break;
+                    case MenuOptions.Exit:
+                        isRunning = false;
+                        break;
+                }
+            }
+        }
+
+        public void InsertContact()
+        {
+
+        }
+
+        public void DeleteContact()
+        {
+
+        }
+
+        public void ModifyContact()
+        {
+
+        }
+
+        public void ViewContact()
+        {
+
+        }
+
+        public void ViewAllContacts()
+        {
+
         }
     }
 }
